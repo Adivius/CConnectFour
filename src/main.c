5c551af4f7279a110b7b5053eceebf7b1c286b96
adivius <adivius@chaos-siegen.de>
2023-10-09 01:36:53 +0200

    Added grid snapping && Added commenting

--- src/main.c
+++ src/main.c
@@ -3,23 +3,35 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
 
-#define COLOR_BACKGROUND 19, 21, 22, 255  // Background color in RGBA format
+// Define constants
+#define COLOR_BACKGROUND 19, 21, 22, 255
+#define MAX_PIECES 42
+#define GRID_SIZE 80 // Size of the grid in pixels
 
 #define WINDOW_WIDTH 1600
 #define WINDOW_HEIGHT 900
 
-#define randNum(min, max) ((rand() % (int)(((max) + 1) - (min))) + (min))
-#define randBool randNum(0, 1)
-
+// Declare global variables and SDL resources
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-SDL_Surface *surface1 = NULL;
-SDL_Texture *texture1 = NULL;
+SDL_Texture *texture = NULL;
 
-int game_is_running = 0;  // Flag to control the game loop
-int last_frame_time = 0;  // Store the time of the last frame
+// Define a structure to hold game pieces
+typedef struct {
+    SDL_Texture *texture;
+    SDL_Rect position;
+} GAME_Piece;
 
-// Function to initialize SDL and create the window and renderer
+GAME_Piece pieces[MAX_PIECES];
+
+// Initialize control variables
+int instruction = 0;
+int player_index = 0;
+int piece_count = 0;
+int game_is_running = 0;
+int last_frame_time = 0;
+
+// Function to initialize SDL and other resources
 int initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL\n");
@@ -48,29 +60,39 @@ int initialize() {
     return 1;
 }
 
-// Function to load an image and create a texture from it
-void setup() {
-    srand(time(NULL));
-
-    surface1 = IMG_Load("textures/player_1.png");
-    if (!surface1) {
+// Function to load the game texture based on the player index
+void loadTexture(int index) {
+    switch (index) {
+        case 1:
+            texture = IMG_LoadTexture(renderer, "textures/player_1.png");
+            break;
+        default:
+            texture = IMG_LoadTexture(renderer, "textures/player_0.png");;
+            break;
+    }
+    if (!texture) {
         fprintf(stderr, "Error loading image: %s\n", IMG_GetError());
         game_is_running = 0;
         return;
     }
-
-    texture1 = SDL_CreateTextureFromSurface(renderer, surface1);
-    if (!texture1) {
-        fprintf(stderr, "Error creating SDL texture: %s\n", SDL_GetError());
-        SDL_FreeSurface(surface1);
-        game_is_running = 0;
-        return;
-    }
-
-    SDL_FreeSurface(surface1);
 }
 
-// Function to process SDL events (e.g., quitting the game)
+// Initialize game resources
+void setup() {
+    srand(time(NULL));
+
+    loadTexture(1);
+}
+
+// Function to snap a position to the grid
+void snapToGrid(SDL_Rect *position) {
+    // Calculate the nearest grid position by adding GRID_SIZE / 2 to center it
+    // and then dividing by GRID_SIZE, ensuring it snaps to the grid
+    position->x = ((position->x + GRID_SIZE / 2) / GRID_SIZE) * GRID_SIZE;
+    position->y = ((position->y + GRID_SIZE / 2) / GRID_SIZE) * GRID_SIZE;
+}
+
+// Process user input and events
 void process() {
     SDL_Event event;
     SDL_PollEvent(&event);
@@ -84,52 +106,83 @@ void process() {
                 case SDLK_ESCAPE:
                     game_is_running = 0;
                     break;
+                case SDLK_r:
+                    piece_count = 0;
+                    instruction = 1;
+                    break;
             }
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+
+            if (event.button.button != SDL_BUTTON_LEFT) {
+                break;
+            }
+
+            if (piece_count < MAX_PIECES) {
+                player_index = !player_index;
+
+                loadTexture(player_index);
+
+                GAME_Piece piece;
+                piece.texture = texture;
+                int textureWidth, textureHeight;
+                SDL_QueryTexture(piece.texture, NULL, NULL, &textureWidth, &textureHeight);
+                SDL_GetMouseState(&piece.position.x, &piece.position.y);
+
+                // Snap the piece's position to the grid
+                snapToGrid(&piece.position);
+
+                piece.position.w = textureWidth;
+                piece.position.h = textureHeight;
+                pieces[piece_count] = piece;
+                piece_count++;
+            }
+
             break;
     }
 }
 
-// Function to update the game state (not used in this example)
+// Update game logic
 void update() {
-    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
-    last_frame_time = SDL_GetTicks();
+//    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
+//    last_frame_time = SDL_GetTicks();
 }
 
-// Function to render the game scene
+// Render game graphics
 void render() {
     SDL_SetRenderDrawColor(renderer, COLOR_BACKGROUND);
     SDL_RenderClear(renderer);
 
-    int textureWidth, textureHeight;
-    SDL_QueryTexture(texture1, NULL, NULL, &textureWidth, &textureHeight);
-
-    SDL_Rect destRect = {0, 0, textureWidth, textureHeight};
-    SDL_RenderCopy(renderer, texture1, NULL, &destRect);
+    if (instruction == 0) {
+        for (int i = 0; i < piece_count; i++) {
+            SDL_RenderCopy(renderer, pieces[i].texture, NULL, &pieces[i].position);
+        }
+    } else {
+        instruction = 0;
+    }
 
     SDL_RenderPresent(renderer);
 }
 
-// Function to clean up resources and quit SDL
+// Clean up resources and quit the game
 void quit() {
-    SDL_DestroyTexture(texture1);
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-// Main function
 int main() {
-    game_is_running = initialize();  // Initialize SDL
-
-    setup();  // Load image and create texture
+    game_is_running = initialize();
+    setup();
 
     while (game_is_running) {
-        process();  // Process SDL events
-        update();   // Update game state (not used in this example)
-        render();   // Render the game scene
+        process();
+        update();
+        render();
     }
 
-    quit();  // Clean up resources and quit SDL
-
+    quit();
     return 0;
 }
