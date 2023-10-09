5e0e4e7a6aac0e340ac4b781cd84a928bdbee9a2
adivius <adivius@chaos-siegen.de>
2023-10-09 19:38:49 +0200

    Removed consts.h && Improved grid snapping && Added preview

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-gcc -Wall -std=c99 ./src/*.c -o cconnectfour -lSDL2 -lSDL2_image && ./cconnectfour
+gcc -Wall -std=c99 ./src/*.c -o cconnectfour -lSDL2 -lSDL2_image &&
+ # x86_64-w64-mingw32-gcc -Wall -std=c99 ./src/*.c -o cconnectfour.exe -lSDL2 -lSDL2_image
+ ./cconnectfour
--- src/main.c
+++ src/main.c
@@ -3,53 +3,57 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
 
-// Define constants
+// Constants
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "Connect Four"
+#define WINDOW_ICON "textures/icon.png"
+
 #define COLOR_BACKGROUND 19, 21, 22, 255
-#define MAX_PIECES 42
-#define GRID_SIZE 80 // Size of the grid in pixels
+#define COLOR_GRID 117, 117, 117, 117
 
-#define WINDOW_WIDTH 1600
-#define WINDOW_HEIGHT 900
+#define ROW_MAX 6
+#define COL_MAX 7
+#define CELL_SIZE 80
+#define GRID_OFFSET_X ((WINDOW_WIDTH - COL_MAX * CELL_SIZE) / 2)
+#define GRID_OFFSET_Y ((WINDOW_HEIGHT - ROW_MAX * CELL_SIZE) / 2)
 
-// Declare global variables and SDL resources
+// SDL Resources
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-SDL_Texture *texture = NULL;
+SDL_Surface *icon = NULL;
+SDL_Texture *textures[3] = {NULL}; // Index 0 for player 1, 1 for player 2, 2 for preview
 
-// Define a structure to hold game pieces
-typedef struct {
-    SDL_Texture *texture;
-    SDL_Rect position;
-} GAME_Piece;
-
-GAME_Piece pieces[MAX_PIECES];
-
-// Initialize control variables
-int instruction = 0;
-int player_index = 0;
-int piece_count = 0;
+int board[ROW_MAX][COL_MAX] = {0};
+int player_index = 1;
 int game_is_running = 0;
-int last_frame_time = 0;
 
 // Function to initialize SDL and other resources
 int initialize() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL\n");
-        printf("%s", SDL_GetError());
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
         return 0;
     }
 
-    if (IMG_Init(IMG_INIT_PNG) == 0) {
-        fprintf(stderr, "Error initializing IMG\n");
+    if (IMG_Init(IMG_INIT_PNG) < 0) {
+        fprintf(stderr, "Error initializing IMG: %s\n", IMG_GetError());
         return 0;
     }
 
-    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,
                               SDL_WINDOW_SHOWN);
     if (!window) {
         fprintf(stderr, "Error creating SDL Window: %s\n", SDL_GetError());
         return 0;
     }
+
+    icon = IMG_Load(WINDOW_ICON);
+    if (!icon) {
+        fprintf(stderr, "Error loading window icon surface: %s\n", IMG_GetError());
+        return 0;
+    }
+
+    SDL_SetWindowIcon(window, icon);
 
     renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
@@ -60,36 +64,47 @@ int initialize() {
     return 1;
 }
 
-// Function to load the game texture based on the player index
-void loadTexture(int index) {
-    switch (index) {
-        case 1:
-            texture = IMG_LoadTexture(renderer, "textures/player_1.png");
-            break;
-        default:
-            texture = IMG_LoadTexture(renderer, "textures/player_0.png");;
-            break;
+// Load textures for players and preview
+int loadTextures() {
+    const char *textureFiles[3] = {"textures/player_0.png", "textures/player_1.png", "textures/preview.png"};
+
+    for (int i = 0; i < 3; i++) {
+        textures[i] = IMG_LoadTexture(renderer, textureFiles[i]);
+        if (!textures[i]) {
+            fprintf(stderr, "Error loading texture %d: %s\n", i, IMG_GetError());
+            return 0;
+        }
     }
-    if (!texture) {
-        fprintf(stderr, "Error loading image: %s\n", IMG_GetError());
-        game_is_running = 0;
-        return;
-    }
+
+    return 1;
 }
 
 // Initialize game resources
 void setup() {
     srand(time(NULL));
 
-    loadTexture(1);
+    if (!loadTextures()) {
+        game_is_running = 0;
+    }
 }
 
-// Function to snap a position to the grid
-void snapToGrid(SDL_Rect *position) {
-    // Calculate the nearest grid position by adding GRID_SIZE / 2 to center it
-    // and then dividing by GRID_SIZE, ensuring it snaps to the grid
-    position->x = ((position->x + GRID_SIZE / 2) / GRID_SIZE) * GRID_SIZE;
-    position->y = ((position->y + GRID_SIZE / 2) / GRID_SIZE) * GRID_SIZE;
+void printBoard() {
+    for (int row = 0; row < ROW_MAX; row++) {
+        for (int col = 0; col < COL_MAX; col++) {
+            printf("%d ", board[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+int dropPiece(int col) {
+    for (int row = ROW_MAX - 1; row >= 0; --row) {
+        if (board[row][col] == 0 || board[row][col] == 3) {
+            board[row][col] = player_index;
+            return row;
+        }
+    }
+    return -1; // Column is full
 }
 
 // Process user input and events
@@ -106,60 +121,101 @@ void process() {
                 case SDLK_ESCAPE:
                     game_is_running = 0;
                     break;
-                case SDLK_r:
-                    piece_count = 0;
-                    instruction = 1;
-                    break;
             }
             break;
-
         case SDL_MOUSEBUTTONDOWN:
+            if (event.button.button == SDL_BUTTON_LEFT) {
 
-            if (event.button.button != SDL_BUTTON_LEFT) {
+                int mouseX = event.button.x;
+
+                int gridX = (mouseX - GRID_OFFSET_X) / CELL_SIZE;
+
+                if (gridX < 0 || gridX >= COL_MAX) {
+                    break;
+                }
+
+                int gridY = dropPiece(gridX);
+                if (gridY == -1) {
+                    break;
+                }
+
+                board[gridY][gridX] = player_index;
+                player_index = player_index == 1 ? 2 : 1;
+
+
+                gridY = dropPiece(gridX);
+                if (gridY == -1) {
+                    break;
+                }
+
+                for (int row = 0; row < ROW_MAX; row++) {
+                    for (int col = 0; col < COL_MAX; col++) {
+                        if (board[row][col] == 3) {
+                            board[row][col] = 0;
+                        }
+                    }
+                }
+                board[gridY][gridX] = 3;
+            }
+            break;
+        case SDL_MOUSEMOTION:
+
+            int mouseX = event.button.x;
+
+            int gridX = (mouseX - GRID_OFFSET_X) / CELL_SIZE;
+
+            if (gridX < 0 || gridX >= COL_MAX) {
                 break;
             }
 
-            if (piece_count < MAX_PIECES) {
-                player_index = !player_index;
-
-                loadTexture(player_index);
-
-                GAME_Piece piece;
-                piece.texture = texture;
-                int textureWidth, textureHeight;
-                SDL_QueryTexture(piece.texture, NULL, NULL, &textureWidth, &textureHeight);
-                SDL_GetMouseState(&piece.position.x, &piece.position.y);
-
-                // Snap the piece's position to the grid
-                snapToGrid(&piece.position);
-
-                piece.position.w = textureWidth;
-                piece.position.h = textureHeight;
-                pieces[piece_count] = piece;
-                piece_count++;
+            int gridY = dropPiece(gridX);
+            if (gridY == -1) {
+                break;
             }
 
+            for (int row = 0; row < ROW_MAX; row++) {
+                for (int col = 0; col < COL_MAX; col++) {
+                    if (board[row][col] == 3) {
+                        board[row][col] = 0;
+                    }
+                }
+            }
+            board[gridY][gridX] = 3;
             break;
     }
 }
 
 // Update game logic
 void update() {
-//    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
-//    last_frame_time = SDL_GetTicks();
 }
+
 
 // Render game graphics
 void render() {
     SDL_SetRenderDrawColor(renderer, COLOR_BACKGROUND);
     SDL_RenderClear(renderer);
 
-    if (instruction == 0) {
-        for (int i = 0; i < piece_count; i++) {
-            SDL_RenderCopy(renderer, pieces[i].texture, NULL, &pieces[i].position);
+    for (int row = 0; row < ROW_MAX; row++) {
+        for (int col = 0; col < COL_MAX; col++) {
+
+            SDL_Rect cellRect = {GRID_OFFSET_X + col * CELL_SIZE, GRID_OFFSET_Y + row * CELL_SIZE, CELL_SIZE,
+                                 CELL_SIZE};
+
+            SDL_SetRenderDrawColor(renderer, COLOR_GRID);
+            SDL_RenderDrawRect(renderer, &cellRect);
+
+            switch (board[row][col]) {
+                case 1:
+                    SDL_RenderCopy(renderer, textures[0], NULL, &cellRect);
+                    break;
+                case 2:
+                    SDL_RenderCopy(renderer, textures[1], NULL, &cellRect);
+                    break;
+                case 3:
+                    SDL_RenderCopy(renderer, textures[2], NULL, &cellRect);
+                    break;
+            }
         }
-    } else {
-        instruction = 0;
     }
 
     SDL_RenderPresent(renderer);
@@ -167,9 +223,12 @@ void render() {
 
 // Clean up resources and quit the game
 void quit() {
-    SDL_DestroyTexture(texture);
+    for (int i = 0; i < 3; i++) {
+        SDL_DestroyTexture(textures[i]);
+    }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
     SDL_Quit();
 }
 
