21be708f6a578068bd4baa663a50949034c416c8
adivius <adivius@chaos-siegen.de>
2023-11-06 23:24:28 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-gcc -Wall ./src/*.c -o cconnectfour -lSDL2 -lSDL2_image -lSDL2_ttf &&
- # x86_64-w64-mingw32-gcc -Wall -std=c99 ./src/*.c -o cconnectfour.exe -lSDL2 -lSDL2_image
- ./cconnectfour 25565
+gcc ./src/*.c -o cconnectfour -lSDL2 -lSDL2_image -lSDL2_ttf
